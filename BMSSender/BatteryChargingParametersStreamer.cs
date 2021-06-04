ae0f0550bc9b6c570ba9d1fd0580ce87bf48877b
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-06-04 17:57:38 +0530

    Create BatteryChargingParametersStreamer.cs

---
+++ BMSSender/BatteryChargingParametersStreamer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BMSSender
+{
+    public class BatteryChargingParametersStreamer : IStreamer
+    {
+        public static bool IsParameterListEmpty(List<IGenerator> chargingparameterlist)
+        {
+            return (!chargingparameterlist.Any());
+        }
+        public void DisplayBatteryChargingParameter(List<IGenerator> chargingparameter)
+        {
+            if (!IsParameterListEmpty(chargingparameter))
+            {
+                Console.WriteLine("Battery Charging Parameters (Press Escape to exit)\n");
+                do
+                {
+                    while (!Console.KeyAvailable)
+                    {
+                        Console.WriteLine("Temperature : {0}\tState of Charge : {1}",
+                            chargingparameter[0].GenerateBatteryChargingParameter(BatteryParametersConstants.minimumChargingTemprature_Celsius,
+                                                                         BatteryParametersConstants.maximumChargingTemprature_Celsius),
+
+                            chargingparameter[1].GenerateBatteryChargingParameter(BatteryParametersConstants.minimumStateOfCharge_Percentage,
+                                                                 BatteryParametersConstants.maximumStateOfCharge_Percentage)
+                            );
+                        Thread.Sleep(2000);
+                    }
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            }
+        }
+    }
+}
